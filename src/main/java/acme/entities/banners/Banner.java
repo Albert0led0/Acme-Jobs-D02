147a008aa6db8f6f87deccd43167e2d3fa7010c0
mpg97 <migpatgut@alum.us.es>
2019-11-06 20:53:43 +0100

    Task-012: finished, correction on Banner

--- src/main/java/acme/entities/banners/Banner.java
+++ src/main/java/acme/entities/banners/Banner.java
@@ -13,7 +13,7 @@ import lombok.Setter;
 @Entity
 @Getter
 @Setter
-public class Banner extends DomainEntity {
+public abstract class Banner extends DomainEntity {
 
 	//Serialisation identifier -------------------------------------------
 
